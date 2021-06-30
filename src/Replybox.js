988e497e000620320ceca942d57cfc72477b8855
ivan-suarez <heyitsivan@protonmail.com>
2021-06-30 23:12:02 +0200

    Improved posts: time, date, and name

--- src/Replybox.js
+++ src/Replybox.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default class Replybox extends React.Component{
     state = {
-        name: 'anonymous',
+        name: 'Anonymous',
         date: "01/01/19",
         hour: "23:04:23",
         number: 1,
@@ -11,17 +11,34 @@ export default class Replybox extends React.Component{
     }
 
     handleChangeName = event => {
-        this.setState({name: event.target.value});
+        let nameVar = event.target.value
+        if(nameVar === "")
+            nameVar = "Anonymous"
+        this.setState({name: nameVar});
     }
     handleChangeText = event => {
         this.setState({text: event.target.value});
     }
+
+    textCountAdd = () => {
+        let num = this.state.number
+        num++
+        this.setState({number: num})
+    }
     handleSubmit = event => {
         event.preventDefault();
+
+        let time = new Date()
+        let today = time.getUTCDay()+"/"+time.getUTCMonth() + "/" + time.getUTCFullYear()
+        let now = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
+        console.log(today)
+        console.log(now)
+        
+        this.textCountAdd()
         const post = {
             name: this.state.name,
-            date: this.state.date,
-            hour: this.state.hour,
+            date: today,
+            hour: now,
             number: this.state.number,
             text: this.state.text,
             
