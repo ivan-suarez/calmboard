4c8ffc0a26eeb90111a73be9b05dbcb755b96567
ivan-suarez <heyitsivan@protonmail.com>
2021-07-13 13:14:30 +0200

    Added routers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Feed from './Feed';
 import PostList from './PostList';
 import Replybox from './Replybox';
+import Catalog from './Catalog'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
 
 function App() {
 
@@ -9,8 +12,31 @@ function App() {
   return (
     <div className="App">
       
+
+
+      <Router>
+    <div className="App">
+     {/** Header */}
+      <Switch>
+        <Route path="/catalog">
+        {/** Header */}
+      
+      {/** Home */}
+      
+      <Catalog/>
+      </Route>
+
+      <Route path="/">
+        {/** Header */}
+      
+      {/** Home */}
       <PostList/>
       <Replybox/>
+      </Route>
+      </Switch>
+      
+    </div>
+    </Router>
     </div>
   );
 }
---
+++ src/Catalog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Catalog() {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default Catalog
